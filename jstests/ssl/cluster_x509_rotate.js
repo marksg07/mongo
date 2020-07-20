d987d790c5e207b67eab5c5a82e01396892a2697
Gabriel Marks <gabriel.marks@mongodb.com>
2020-07-20 21:50:29 +0000

    SERVER-49749 Test rotate functionality in various cases

---
+++ jstests/ssl/cluster_x509_rotate.js
@@ -0,0 +1,108 @@
+// Check that rotation works for the cluster certificate
+
+(function() {
+    "use strict";
+
+    load('jstests/ssl/libs/ssl_helpers.js');
+    
+    if (determineSSLProvider() === "openssl") {    
+        return;
+    }
+    
+    function winpath(p) {
+        return p.replace(/\//g, "\\");
+    }
+    
+    function copyfile(a, b) {
+        if(_isWindows()) {
+            return assert.eq(0, runProgram("cmd.exe", "/c", "copy", winpath(a), winpath(b)));
+        }
+        return assert.eq(0, runProgram("cp", a, b));
+    }
+
+    let mongos;
+    function getConnPoolHosts() {
+        const ret = mongos.adminCommand({connPoolStats: 1});
+        assert.commandWorked(ret);
+        jsTestLog("Connection pool stats by host: " + tojson(ret.hosts));
+        return ret.hosts;
+    }
+    
+    const dbPath = MongoRunner.toRealDir("$dataDir/cluster_x509_rotate_test/");
+    mkdir(dbPath);
+    
+    copyfile("jstests/libs/ca.pem", dbPath + "/ca-test.pem");
+    copyfile("jstests/libs/client.pem", dbPath + "/client-test.pem");
+    copyfile("jstests/libs/server.pem", dbPath + "/server-test.pem");
+    
+    // server certificate is held constant so that shell can still connect
+    const mongosOptions = {
+        sslMode: "requireSSL",
+        sslPEMKeyFile: "jstests/libs/server.pem",
+        sslCAFile: dbPath + "/ca-test.pem",
+        sslClusterFile: dbPath + "/client-test.pem",
+        sslAllowInvalidHostnames: "",
+    };
+    
+    const configOptions = {
+        sslMode: "requireSSL",
+        sslPEMKeyFile: dbPath + "/server-test.pem",
+        sslCAFile: dbPath + "/ca-test.pem",
+        sslAllowInvalidHostnames: "",
+    }
+    
+    const sharding_config = {
+        config: 1,
+        mongos: 1,
+        shards: 3,
+        other: {
+            configOptions: configOptions,
+            mongosOptions: mongosOptions,
+            rsOptions: configOptions,
+            shardOptions: configOptions,
+        }
+    };
+
+    let st = new ShardingTest(sharding_config);
+    mongos = st.s0;
+    // Make sure all hosts are known
+    assert.soon(() => {
+        mongos.adminCommand({multicast: {ping: 0}});
+        return true;
+    });
+    
+    const rst = st.rs0;
+    const primary = rst.getPrimary();
+
+    // Since we pinged, the primary should be present
+    assert(primary.host in getConnPoolHosts());
+
+    // Drop connection to the primary before killing it
+    mongos.adminCommand({dropConnections: 1, hostAndPort: [primary.host]});
+    assert(!(primary.host in getConnPoolHosts()));
+    
+    // Swap out the certificate files and rotate. If rotate works, mongos should be able to connect to the restarted shard.
+    copyfile("jstests/libs/trusted-ca.pem", dbPath + "/ca-test.pem");
+    copyfile("jstests/libs/trusted-client.pem", dbPath + "/client-test.pem");
+    copyfile("jstests/libs/trusted-server.pem", dbPath + "/server-test.pem");
+    assert.soon(() => {
+        mongos.adminCommand({rotateCertificates: 1});
+        return true;
+    });
+    
+    // Can't use .restart since waitForConnect must be false. Otherwise, this would hang as the shard has an unmatching certificate.
+    assert.soon(() => {
+        rst.stop(primary, undefined, undefined, {forRestart: true});
+        rst.start(primary, Object.assign({}, configOptions, {waitForConnect: false}), true);
+        return true;
+    });
+    
+    assert.soon(() => {
+        mongos.adminCommand({multicast: {ping: 0}});
+        return true;
+    });
+
+    // Check for successful connection, indicating rotation successful
+    assert(primary.host in getConnPoolHosts());
+    }());
+    
---
+++ jstests/ssl/server_x509_rotate.js
@@ -0,0 +1,50 @@
+// Check that rotation works for the cluster certificate
+
+(function() {
+    "use strict";
+
+    load('jstests/ssl/libs/ssl_helpers.js');
+    
+    if (determineSSLProvider() === "openssl") {    
+        return;
+    }
+    
+    function winpath(p) {
+        return p.replace(/\//g, "\\");
+    }
+    
+    function copyfile(a, b) {
+        if(_isWindows()) {
+            return assert.eq(0, runProgram("cmd.exe", "/c", "copy", winpath(a), winpath(b)));
+        }
+        return assert.eq(0, runProgram("cp", a, b));
+    }
+
+    const dbPath = MongoRunner.toRealDir("$dataDir/cluster_x509_rotate_test/");
+    mkdir(dbPath);
+    
+    copyfile("jstests/libs/ca.pem", dbPath + "/ca-test.pem");
+    copyfile("jstests/libs/client.pem", dbPath + "/client-test.pem");
+    copyfile("jstests/libs/server.pem", dbPath + "/server-test.pem");
+
+    let mongod = MongoRunner.runMongod({
+        sslMode: "requireSSL",
+        sslPEMKeyFile: dbPath + "/server-test.pem",
+        sslCAFile: dbPath + "/ca-test.pem"
+    });
+
+    // Rotate in new certificates
+    copyfile("jstests/libs/trusted-ca.pem", dbPath + "/ca-test.pem");
+    copyfile("jstests/libs/trusted-client.pem", dbPath + "/client-test.pem");
+    copyfile("jstests/libs/trusted-server.pem", dbPath + "/server-test.pem");
+
+    mongod.adminCommand({rotateCertificates: 1});
+
+    // Start shell with old certificates and make sure it can't connecty
+    let out = runMongoProgram("mongo", "--ssl", "--sslPEMKeyFile", "jstests/libs/client.pem", "--sslCAFile", "jstests/libs/ca.pem", "--eval", ";");
+    assert.neq(out, 0, "Mongo invocation did not fail");
+
+    // Start shell with new certificates and make sure it can connect
+    let out = runMongoProgram("mongo", "--ssl", "--sslPEMKeyFile", dbPath + "/client-test.pem", "--sslCAFile", dbPath + "/ca-test.pem", "--eval", ";");
+    assert.eq(out, 0, "Mongo invocation failed");
+}());
